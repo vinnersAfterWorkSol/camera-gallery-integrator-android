85743be8126d0b916839e4127b2d5336273e0027
Himanshu <himanshu.temp@gig4ce.com>
2024-06-18 16:27:31 +0530

    Compile Options Updated

--- cameraIntegrator/build.gradle.kts
+++ cameraIntegrator/build.gradle.kts
@@ -23,8 +23,12 @@ android {
             )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
